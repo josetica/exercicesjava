f742bb859423d555f8a67fa06227ed338459565e
josetica <77306079+josetica@users.noreply.github.com>
2021-03-30 01:19:03 +0200

    Add files via upload

---
+++ Mois.java
@@ -0,0 +1,14 @@
+import java.util.Calendar;
+public class Mois {
+
+  public static void main(String[] args) {
+    
+   String mois[] = {"janvier","fevrier","mars","avril","avril", "mai", "juin", "juillet","aout", "septembre","octobre","novembre", "deecembre"};
+
+    String mois1;
+   mois1 = mois[cal.get(Calendar. MONTH)];
+   System.out.println("MOIS1");
+  }
+
+}
+
