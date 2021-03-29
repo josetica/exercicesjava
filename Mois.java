49a3a450f9c79496b08749e9cab7101919488edc
josetica <77306079+josetica@users.noreply.github.com>
2021-03-30 01:34:35 +0200

    Add files via upload

--- Mois.java
+++ Mois.java
@@ -1,14 +1,49 @@
-import java.util.Calendar;
-public class Mois {
-
-  public static void main(String[] args) {
-    
-   String mois[] = {"janvier","fevrier","mars","avril","avril", "mai", "juin", "juillet","aout", "septembre","octobre","novembre", "deecembre"};
-
-    String mois1;
-   mois1 = mois[cal.get(Calendar. MONTH)];
-   System.out.println("MOIS1");
-  }
-
+import java.util.Scanner;
+class Mois{
+    public static void main(String[] args) {
+        Scanner myObj= new Scanner(System.in);
+        System.out.print("Entrer le mois en chiffre : ");
+        int mois=myObj.nextInt(); 
+        switch (mois) {
+            case 1:
+                System.out.println("Janvier");
+                break;
+            case 2:
+                System.out.println("Février");
+                break;
+            case 3:
+                System.out.println("Mars");
+                break;
+            case 4:
+                System.out.println("Avril");
+                break;
+            case 5:
+                System.out.println("Mai");
+                break;
+            case 6:
+                System.out.println("Juin");
+                break;
+            case 7:
+                System.out.println("Juillet");
+                break;
+            case 8:
+                System.out.println("Aout");
+                break;
+            case 9:
+                System.out.println("Septembre");
+                break;
+            case 10:
+                System.out.println("Octobre");
+                break;
+            case 11:
+                System.out.println("Novembre");
+                break;
+            case 12:
+                System.out.println("Décembre");
+                break;
+            default:
+                System.out.println("Error ! Entrer un chiffre entre 1 et 12 ");
+                break;
+        }
+    }
 }
-
