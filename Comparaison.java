c29d96590b0a5567eca773216ab4aa8d989d6d39
josetica <77306079+josetica@users.noreply.github.com>
2021-03-30 01:19:50 +0200

    Add files via upload

---
+++ Comparaison.java
@@ -0,0 +1,14 @@
+class Comparaison {
+    
+    public static void main(String[] args){
+        int a=2;
+        int b=4;
+        if(a == b){
+            System.out.println("Egaux");
+        } else{
+            System.out.println("Differents");
+        }
+    }
+
+    }
+        
