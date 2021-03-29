7068aef864aa7f85745bf496324783e19f797154
josetica <77306079+josetica@users.noreply.github.com>
2021-03-30 01:20:19 +0200

    Add files via upload

---
+++ Ordre.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+class Ordre {
+public static void main(String[] args)
+{
+    int a=15;
+    int b=20;
+    int c=27;  
+System.out.println("Trois entiers : a, b, c ");
+System.out.println("le nombre a :  " +a);
+System.out.println("le nombre b :  " +b);
+System.out.println("le nombre c :  " +c);
+if (a>b && b>c && c>a){
+    System.out.println("ordre decroissant: c, b, a " );
+}
+if (b>a && c>b && a>c){
+    System.out.println("ordre decroissant: b, c, a " );
+}
+if ()
+
+
+
+
+
+
+
+
+
+
+  
+
+}
+
+ 
+}
